4916f4d63d9cc1e257326443df637a441f5d97c0
Jhr <Jhr@cqwu.edu>
2023-01-20 15:48:02 +0800

    1、新增注册用户接口

--- diary-service/src/main/java/cqwu/edu/diary/service/service/ISysUserService.java
+++ diary-service/src/main/java/cqwu/edu/diary/service/service/ISysUserService.java
@@ -1,13 +1,28 @@
 package cqwu.edu.diary.service.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import cqwu.edu.diary.common.dto.RegisterDTO;
 import cqwu.edu.diary.common.entity.SysUserEntity;
 import cqwu.edu.diary.common.vo.SysUserVO;
 import cqwu.edu.diary.service.mapper.SysUserMapper;
 
 public interface ISysUserService extends IService<SysUserEntity> {
 
+    /**
+     * 通过用户名查找用户
+     * @param username 用户名
+     * @return 用户对象
+     */
     SysUserEntity selectUserByName(final String username);
 
+    /**
+     * 查询当前登录人用户信息
+     */
     SysUserVO queryLoginUser();
+
+    /**
+     * 注册用户
+     * @param dto 注册参数对象
+     */
+    void Register(final RegisterDTO dto);
 }
--- diary-service/src/main/java/cqwu/edu/diary/service/service/impl/SysUserServiceImpl.java
+++ diary-service/src/main/java/cqwu/edu/diary/service/service/impl/SysUserServiceImpl.java
@@ -1,18 +1,33 @@
 package cqwu.edu.diary.service.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import cqwu.edu.diary.common.dto.RegisterDTO;
 import cqwu.edu.diary.common.entity.SysUserEntity;
+import cqwu.edu.diary.common.utils.SnowflakeDistributeId;
 import cqwu.edu.diary.common.vo.SysUserVO;
+import cqwu.edu.diary.service.config.SecurityConfig;
 import cqwu.edu.diary.service.mapper.SysUserMapper;
+import cqwu.edu.diary.service.security.encode.PasswordEncoder;
 import cqwu.edu.diary.service.security.util.SecurityUtil;
 import cqwu.edu.diary.service.service.ISysUserService;
 import org.springframework.beans.BeanUtils;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 
 @Service("sysUserService")
 public class SysUserServiceImpl extends ServiceImpl<SysUserMapper,SysUserEntity> implements ISysUserService{
 
+    @Resource
+    private SysUserMapper sysUserMapper;
+
+    /**
+     * 通过用户名查找用户
+     * @param username 用户名
+     * @return 用户对象
+     */
     @Override
     public SysUserEntity selectUserByName(String username) {
         return lambdaQuery().eq(SysUserEntity::getUsername,username)
@@ -20,6 +35,9 @@ public class SysUserServiceImpl extends ServiceImpl<SysUserMapper,SysUserEntity>
                 .one();
     }
 
+    /**
+     * 查询当前登录人用户信息
+     */
     @Override
     public SysUserVO queryLoginUser() {
         final Long id = SecurityUtil.getId();
@@ -27,5 +45,18 @@ public class SysUserServiceImpl extends ServiceImpl<SysUserMapper,SysUserEntity>
         final SysUserVO userVO = new SysUserVO();
         BeanUtils.copyProperties(userEntity,userVO);
         return userVO;
+    }
+
+    /**
+     * 注册用户
+     * @param dto 注册参数对象
+     */
+    @Override
+    public void Register(RegisterDTO dto) {
+        final SysUserEntity userEntity = new SysUserEntity();
+        userEntity.setId(SnowflakeDistributeId.getInstance().nextId());
+        final String password = new BCryptPasswordEncoder().encode(dto.getPassword());
+        userEntity.setPassword(password);
+        sysUserMapper.insert(userEntity);
     }
 }
